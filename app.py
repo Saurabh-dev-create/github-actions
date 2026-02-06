44170a34c4eb2594a4fb0075674d52585465dc84
Saurabh@Kepler <Saurabh@Kepler>
2026-02-07 04:24:29 +0530

    Small change in app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "<h1>Hi from github_actions</h1>"
+    return "<h1>Hi from Github_Actions</h1>"
 if __name__=="__main__":
     app.run(port=5000,host="0.0.0.0")
